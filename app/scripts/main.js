57c57d68b2974490101a9a8e0df6d5f6ae3ab59c
Chelsea Franz <chelseafranz3@gmail.com>
2014-10-07 14:05:46 -0400

    finisjed js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,22 @@
-console.log('The Iron Yard Rocks');
+var count = $('#drink'),
+  title, price, currency_code, image, shop_name;
+var glass;
+
+whiskey.results.forEach( function (item) {
+
+title = "<p>" + item.title + "</p>";
+
+price = "<p>" + item.price + "</p>";
+
+currency_code = "<p>" + item.currency_code + "</p>";
+
+shop_name = "<p>" + item.Shop.shop_name + "</p>";
+
+image = "<img src='" + item.Images[0].url_170x135 + "' />";
+
+glass="<li>" + title + price + currency_code + shop_name + image + "</li>";
+
+
+count.append(glass);
+
+});
