8b48b8158e72a571cde5051488ddbb33c6721727
Chelsea Franz <chelseafranz3@gmail.com>
2014-10-08 08:55:59 -0400

    morning

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -18,8 +18,7 @@ image = "<img src='" + item.Images[0].url_170x135 + "' />";
 box = "<img class='box' src='" + 'https://raw.githubusercontent.com/tiy-atlanta-js/Assignments/master/Assignment%2008/assets/hamburger.png' + "' />"+"<img class='box2' src='" + 'https://raw.githubusercontent.com/tiy-atlanta-js/Assignments/master/Assignment%2008/assets/heart.png' + "' />";
 
 
-glass="<li>" + box + image + title + shop_name +  currency_code + price +"</li>";
-
+glass="<li class='mainList'>" + box + image + title + shop_name +  currency_code + price +"</li>";
 
 count.append(glass);
 
